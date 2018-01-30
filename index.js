a62a5eb4762b26b87c7ba5086c28827a64cdec54
Your Name <you@example.com>
2018-01-30 15:28:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+//let app = require('express')();
+let http = require('http').Server(express);
+let io = require('socket.io')(http);
+ 
+io.on('connection', (socket) => {
+  
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+  });
+ 
+  socket.on('set-nickname', (nickname) => {
+    socket.nickname = nickname;
+    io.emit('users-changed', {user: nickname, event: 'joined'});    
+  });
+  
+  socket.on('add-message', (message) => {
+    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  });
+});
+ 
+//var port = process.env.PORT || 3001;
+ 
+http.listen(PORT, function(){
+   console.log('listening in http://localhost:' + PORT);
+});
